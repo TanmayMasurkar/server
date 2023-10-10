35ad0245e86e1224dd8185db3b289635fd85768f
TanmayMasurkar <tanmaymasurkar2002@gmail.com>
2023-10-10 09:58:23 +0530

    update file

--- router/user.router.js
+++ router/user.router.js
@@ -5,15 +5,13 @@ import { authenticate, isAdmin } from "../middleware/auth";
 
 const router = express.Router();
 
-router.get("/", getUserRecords);
-
+// admin 
+router.get("/", authenticate, isAdmin, getUserRecords);
 router.post('/add-user', authenticate, isAdmin, newUser);
 
-router.get("/:user_id",authenticate, isAdmin, getSingleUser);
-
-router.delete("/:user_id",authenticate, isAdmin, deleteUser);
-
-router.put('/:user_id',authenticate, isAdmin,updateUser)
+router.get("/:user_id",authenticate, getSingleUser);
+router.delete("/:user_id",authenticate, deleteUser);
+router.put('/:user_id',authenticate,updateUser)
 
 
 
